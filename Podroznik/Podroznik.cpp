e21eb4063556b4670160cf381a4ef1c0e6ea8964
wardusv3 <wardusv3@DESKTOP-B9UJGEC>
2023-12-18 08:21:01 +0100

    Dodaj pliki projektów.

---
+++ Podroznik/Podroznik.cpp
@@ -0,0 +1,28 @@
+﻿#include<gl/glut.h>
+#include <gl\gl.h>
+#include <gl\freeglut.h>
+
+// Drawing function:
+void OnDraw() {
+    // Background colour (R, G, B, Alpha):
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    // Clearing the colour buffer:
+    glClear(GL_COLOR_BUFFER_BIT);
+    // Flushing the rendering queue:
+    glFinish();	// or glFlush();
+}
+
+int main(int argc, char* argv[]) {
+    glutInit(&argc, argv);
+
+    // Preparing the window:
+    glutInitDisplayMode(GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Hello OpenGL");
+
+    // Drawing callback function:
+    glutDisplayFunc(OnDraw);
+
+    glutMainLoop();
+    return(0);
+}
